8e0f881bdbf5458c696fdbf03eeca4ae5674c2b1
Benny Neugebauer <bn@bennyn.de>
2017-11-07 21:40:14 +0100

    cleanup

--- src/test/node/spec/Account.test.js
+++ src/test/node/spec/Account.test.js
@@ -2,18 +2,18 @@ const {Account} = require('../../../../dist/commonjs/');
 
 describe('Account', () => {
   describe('"constructor"', () => {
-    it('creates a temporary client by default', () => {
+    it('creates a temporary client by default.', () => {
       const account = new Account({email: 'me@wire.com', password: 'secret'});
       expect(account.loginData.persist).toBe(false);
     });
 
-    it('sanitizes login data', () => {
+    it('sanitizes login data.', () => {
       const account = new Account({email: 'me@wire.com\t', password: '\r\nsecret'});
       expect(account.loginData.email).toBe('me@wire.com');
       expect(account.loginData.password).toBe('secret');
     });
 
-    it('turns a given password into a string', () => {
+    it('turns a given password into a string.', () => {
       const account = new Account({email: 'me@wire.com\t', password: 1234567890});
       expect(typeof account.loginData.password).toBe('string');
     });
